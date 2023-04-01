592e7f8b9160839f02c168688c2d6258dab12eb2
KomalT2003 <114212212+KomalT2003@users.noreply.github.com>
2023-04-01 14:17:53 +0530

    Update SafestRoute.jsx

--- MobileApp/Screens/SafestRoute.jsx
+++ MobileApp/Screens/SafestRoute.jsx
@@ -1,20 +1,99 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import MapView, { Marker, Polyline } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
- export default function SafestRoute ({navigation}) {
-  return(
+const origin = {latitude: 28.6692, longitude: 77.107};
+const destination = {latitude: 28.5355, longitude: 77.2649};
+
+const routes = [
+  {origin: {latitude: 28.6692, longitude: 77.107}, destination: {latitude: 28.5355, longitude: 77.2649}},
+  {origin: {latitude: 28.6692, longitude: 77.107}, destination: {latitude: 28.5303, longitude: 77.2672}},
+  {origin: {latitude: 28.6692, longitude: 77.107}, destination: {latitude: 28.5484, longitude: 77.2359}},
+];
+
+const Map = () => {
+  const [region, setRegion] = useState({
+    latitude: 28.6692,
+    longitude: 77.107,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  useEffect(() => {
+    setRegion({
+      latitude: 28.6692,
+      longitude: 77.107,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  }, []);
+  const key="AIzaSyD5puZeCAKP5CnZxPbhvWIezhWdHfJAwtY"
+  return (
     <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={region}
+        showsUserLocation={true}
+        followUserLocation={true}
+      >
+        <Marker coordinate={origin} />
+        <Marker coordinate={destination} />
 
-      <StatusBar style="auto" />
+        {routes.map((route, index) => (
+          <MapViewDirections
+            key={index}
+            origin={route.origin}
+            destination={route.destination}
+            apikey={key}
+            strokeWidth={3}
+            strokeColor="#FF0000"
+            optimizeWaypoints={true}
+            onStart={(params) => {
+              console.log(`Started routing between "${params.origin}" and "${params.destination}"`);
+            }}
+            onReady={(result) => {
+              console.log(`Distance: ${result.distance} km`);
+              console.log(`Duration: ${result.duration} min.`);
+
+              // Uncomment the line below to show the directions in the console
+               console.log(result.steps);
+
+              setRegion({
+                latitude: result.coordinates[0]['latitude'],
+                longitude: result.coordinates[0]['longitude'],
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              });
+            }}
+            onError={(errorMessage) => {
+              // console.log('GOT AN ERROR');
+            }}
+          />
+        ))}
+
+        <Polyline
+          coordinates={[
+            {latitude: 28.6692, longitude: 77.107},
+            {latitude: 28.5355, longitude: 77.2649},
+          ]}
+          strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+          strokeWidth={6}
+        />
+      </MapView>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
+
+export default Map
