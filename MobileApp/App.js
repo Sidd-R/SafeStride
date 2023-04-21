32d73850e995c8361927297b96e2192ccc798c53
Sidd-R <siddhant.rao@spit.ac.in>
2023-04-22 02:31:53 +0530

    v

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,65 +1,136 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer} from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createTheme, ThemeProvider} from'@rneui/themed'
-import Home from './Screens/Home';
-import NearestSafeSpot from './Screens/NearestSafeSpot';
-import Sos from './Screens/Sos';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { NavigationContainer} from '@react-navigation/native';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { createTheme, ThemeProvider} from'@rneui/themed'
+// import Home from './Screens/Home';
+// import NearestSafeSpot from './Screens/NearestSafeSpot';
+// import Sos from './Screens/Sos';
+// import Login from './Screens/Login';
+// import DirectSpot from './Screens/DirectSpot';
+// import SafestRoute from './Screens/SafestRoute';
+// import Register from './Screens/Register';
+
+// // Navigation
+// const Drawer = createDrawerNavigator();
+
+// export default function App() {
+//   const theme = createTheme({
+//     lightColors: {
+//       primary: 'rgb(32, 136, 192)',
+//     },
+//     darkColors: {
+//       primary: 'black',
+//     },
+//     mode: 'light',
+//   });
+
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <NavigationContainer>
+//         <Drawer.Navigator initialRouteName="Home">
+//           {/* <Drawer.Screen name="Home" component={MainStackNavigator} /> */}
+//           <Drawer.Screen name="Home" component={Home} 
+//           options={{
+//             title:"Home",
+//             headerStyle:{
+//               backgroundColor: 'cadetblue',
+//               height:90,  
+//             },
+//             headerStatusBarHeight:25,
+//             headerTintColor:'black'
+//           }}/>
+//           <Drawer.Screen name="Nearest Safe Spot" component={NearestSafeSpot} />
+//           <Drawer.Screen name="Safest Route" component={SafestRoute} />
+//           <Drawer.Screen name="S.O.S" component={Sos} />
+//           <Drawer.Screen name="Login" component={Login} options={{headerShown:false,}}/>
+//           <Drawer.Screen name="DirectSpot" component={DirectSpot} />
+//           <Drawer.Screen name="Register" component={Register} options={{headerShown:false,}}/>
+//         </Drawer.Navigator>
+//       </NavigationContainer>
+//     </ThemeProvider>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     padding: 20,
+//   },
+//   heading: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//     marginBottom: 10,
+//   },
+// });
+
+
+// // Example of Splash, Login and Sign Up in React Native
+// // https://aboutreact.com/react-native-login-and-signup/
+// import 'react-native-gesture-handler';
+
+// Import React and Component
+import React from 'react';
+import { Text } from 'react-native';
+// Import Navigators from React Navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// Import Screens
+import SplashScreen from './Screens/SplashScreen';
 import Login from './Screens/Login';
-import DirectSpot from './Screens/DirectSpot';
-import SafestRoute from './Screens/SafestRoute';
+import Register from './Screens/Register';
+import DrawerNavigationRoutes from './Screens/DrawerNavigationRoutes';
 
-// Navigation
-const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function App() {
-  const theme = createTheme({
-    lightColors: {
-      primary: 'aliceblue',
-    },
-    darkColors: {
-      primary: 'rgb(32, 136, 192)',
-    },
-    mode: 'light',
-  });
-
+const Auth = () => {
+  // Stack Navigator for Login and Sign up Screen
   return (
-    <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Home">
-          {/* <Drawer.Screen name="Home" component={MainStackNavigator} /> */}
-          <Drawer.Screen name="Home" component={Home} 
-          options={{
-            title:"Home",
-            headerStyle:{
-              backgroundColor: 'cadetblue',
-              height:90,  
-            },
-            headerStatusBarHeight:25,
-            headerTintColor:'black'
-          }}/>
-          <Drawer.Screen name="Nearest Safe Spot" component={NearestSafeSpot} />
-          <Drawer.Screen name="Safest Route" component={SafestRoute} />
-          <Drawer.Screen name="S.O.S" component={Sos} />
-          <Drawer.Screen name="Login" component={Login} />
-          <Drawer.Screen name="DirectSpot" component={DirectSpot} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    </ThemeProvider>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  heading: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-});
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        {/* SplashScreen which will come once for 5 Seconds */}
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          // Hiding header for Splash Screen
+          options={{headerShown: false}}
+        />
+        {/* Auth Navigator: Include Login and Signup */}
+        <Stack.Screen
+          name="Auth"
+          component={Auth}
+          options={{headerShown: false}}
+        />
+        {/* Navigation Drawer as a landing page */}
+        <Stack.Screen
+          name="DrawerNavigationRoutes"
+          component={DrawerNavigationRoutes}
+          // Hiding header for Navigation Drawer
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- MobileApp/Screens/Login.jsx
+++ MobileApp/Screens/Login.jsx
@@ -1,9 +1,129 @@
-import { useState,useEffect } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+// import { useState,useEffect } from 'react';
+// import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// import {DATABASE_API_KEY} from '@env'
+// import {Button} from '@rneui/themed'
+// const firebaseConfig = {
+//   apiKey: DATABASE_API_KEY,
+//   authDomain: "safestride-655dd.firebaseapp.com",
+//   projectId: "safestride-655dd",
+//   storageBucket: "safestride-655dd.appspot.com",
+//   messagingSenderId: "485999909084",
+//   appId: "1:485999909084:web:8f37313fe268cf3f6b7c19"
+// };
+
+// export default function Login({navigation}) {
+// 	let db
+//   useEffect(() => {
+// 		const app = initializeApp(firebaseConfig)
+//     db = getFirestore(app)
+// 	}, [])
+	
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [error, setError] = useState('');
+
+//   const handleLogin = async () => { 
+//     // const users = collection(db,'users')
+//     // const usercred = await getDocs(users)
+//     // const temp =  usercred.docs.map(doc => doc.data());
+//     // console.log(temp);
+// 		navigation.navigate('Home')
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Button title={"Login"} type='solid'/>
+//       <Text style={styles.heading}>Login</Text>
+//       <TextInput
+//         style={styles.input}
+//         placeholder="Email"
+//         value={email}
+//         onChangeText={setEmail}
+//         autoCapitalize="none"
+//       />
+//       <TextInput
+//         style={styles.input}
+//         placeholder="Password"
+//         value={password}
+//         onChangeText={setPassword}
+//         secureTextEntry={true}
+//       />
+//       {/* <Butt */}
+//       {error ? <Text style={styles.error}>{error}</Text> : null}
+//       <TouchableOpacity style={styles.button} onPress={handleLogin}>
+//         <Text style={styles.buttonText}>Login</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     padding: 20,
+//     paddingBottom: '40%'
+//   },
+//   heading: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//     marginBottom: 10,
+//   },
+//   input: {
+//     width: '100%',
+//     height: 50,
+//     borderWidth: 1,
+//     borderColor: '#ccc',
+//     borderRadius: 5,
+//     padding: 10,
+//     marginBottom: 15,
+//     fontSize: 16,
+//     marginTop: 10
+//   },
+//   button: {
+//     width: '100%',
+//     height: 50,
+//     backgroundColor: '#3498db',
+//     borderRadius: 5,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     marginTop:60
+//   },
+//   buttonText: {
+//     color: '#fff',
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//   },
+//   error: {
+//     color: 'red',
+//     marginBottom: 10,
+//   },
+// });
+
+import React, {useState, createRef} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Dimensions,
+  ToastAndroid,
+} from 'react-native';
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDoc,doc,setDoc, } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Loader from '../components/Loader';
 import {DATABASE_API_KEY} from '@env'
-import {Button} from '@rneui/themed'
+
 const firebaseConfig = {
   apiKey: DATABASE_API_KEY,
   authDomain: "safestride-655dd.firebaseapp.com",
@@ -13,93 +133,195 @@ const firebaseConfig = {
   appId: "1:485999909084:web:8f37313fe268cf3f6b7c19"
 };
 
-export default function Login({navigation}) {
-	let db
-  useEffect(() => {
-		const app = initializeApp(firebaseConfig)
-    db = getFirestore(app)
-	}, [])
-	
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+const Login = ({navigation}) => {
+  const app = initializeApp(firebaseConfig)
+  const db = getFirestore(app)
 
-  const handleLogin = async () => { 
-    // const users = collection(db,'users')
-    // const usercred = await getDocs(users)
-    // const temp =  usercred.docs.map(doc => doc.data());
-    // console.log(temp);
-		navigation.navigate('Home')
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errortext, setErrortext] = useState('');
+
+  const passwordInputRef = createRef();
+
+  const handleSubmitPress = async () => {
+    setErrortext('');
+    if (!userEmail) {
+      alert('Please fill Email');
+      return;
+    }
+    if (!userPassword) {
+      alert('Please fill Password');
+      return;
+    }
+    setLoading(true);
+    const userId = doc(db,'users',userEmail)
+    const user = await getDoc(userId)
+    if (!user.exists()) {
+      alert('User dosen\'t exists')
+      setLoading(false);
+      return
+    } else if (user.data().password != userPassword) {
+      alert('Incorrect password')
+      setLoading(false);
+      return
+    }
+    ToastAndroid.show('Login Successfull',ToastAndroid.SHORT)
+    setLoading(false);
+    // navigation.navigate('Home')
+    //     console.log(responseJson);
+    //     // If server response message same as Data Matched
+    //     if (responseJson.status === 'success') {
+          await AsyncStorage.setItem('user_id', userEmail);
+          navigation.replace('DrawerNavigationRoutes');
+    //     } else {
+    //       setErrortext(responseJson.msg);
+    //       console.log('Please check your email id or password');
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     //Hide Loader
+    //     setLoading(false);
+    //     console.error(error);
+    //   });
   };
 
   return (
-    <View style={styles.container}>
-      <Button/>
-      <Text style={styles.heading}>Login</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry={true}
-      />
-      {/* <Butt */}
-      {error ? <Text style={styles.error}>{error}</Text> : null}
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
+    <View style={styles.mainBody}>
+      <Loader loading={loading} />
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}>
+        <View>
+          <KeyboardAvoidingView enabled>
+            <View style={{alignItems: 'center'}}>
+              <Image
+                source={require('../assets/SafeStrideLogo.png')}
+                style={{
+                  width: Dimensions.get('window').width*0.7,
+                  height: Dimensions.get('window').width*0.7,
+                  resizeMode: 'contain',
+                  margin: 30,
+                }}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={(UserEmail) =>
+                  setUserEmail(UserEmail)
+                }
+                placeholder="Enter Email" //dummy@abc.com
+                placeholderTextColor="#8b9cb5"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  passwordInputRef.current &&
+                  passwordInputRef.current.focus()
+                }
+                underlineColorAndroid="#f000"
+                blurOnSubmit={false}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={(UserPassword) =>
+                  setUserPassword(UserPassword)
+                }
+                placeholder="Enter Password" //12345
+                placeholderTextColor="#8b9cb5"
+                keyboardType="default"
+                ref={passwordInputRef}
+                onSubmitEditing={Keyboard.dismiss}
+                blurOnSubmit={false}
+                secureTextEntry={true}
+                underlineColorAndroid="#f000"
+                returnKeyType="next"
+              />
+            </View>
+            {errortext != '' ? (
+              <Text style={styles.errorTextStyle}>
+                {errortext}
+              </Text>
+            ) : null}
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              activeOpacity={0.5}
+              onPress={handleSubmitPress}>
+              <Text style={styles.buttonTextStyle}>LOGIN</Text>
+            </TouchableOpacity>
+            <Text
+              style={styles.registerTextStyle}
+              onPress={() => navigation.navigate('Register')}>
+              New Here ? Register
+            </Text>
+          </KeyboardAvoidingView>
+        </View>
+      </ScrollView>
     </View>
   );
-}
+};
+export default Login;
 
 const styles = StyleSheet.create({
-  container: {
+  mainBody: {
     flex: 1,
+    justifyContent: 'center',
     backgroundColor: '#fff',
+    alignContent: 'center',
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#106ffe',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
     alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-    paddingBottom: '40%'
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
   },
-  heading: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  input: {
-    width: '100%',
-    height: 50,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 15,
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
     fontSize: 16,
-    marginTop: 10
   },
-  button: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#3498db',
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop:60
+  inputStyle: {
+    flex: 1,
+    color: '#111',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 7,
+    borderColor: '#dadae8',
   },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
+  registerTextStyle: {
+    color: 'cornflowerblue',
+    textAlign: 'center',
     fontWeight: 'bold',
+    fontSize: 14,
+    alignSelf: 'center',
+    padding: 10,
   },
-  error: {
+  errorTextStyle: {
     color: 'red',
-    marginBottom: 10,
+    textAlign: 'center',
+    fontSize: 14,
   },
 });
